6b60224c3865bdbed574f5d0ea8a686928a759c2
Michael L Parks <github@wmi-inc.com>
2016-08-08 10:37:44 -0400

    Database Seeded

--- waylow/db/seeds.rb
+++ waylow/db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+competition = Competition.create(
+  start_date: "9/8/2015",
+  end_date: "12/8/2016",
+  location: "Woodstream",
+  team_names: "Team 1, Team 2, Team 3",
+  about: "For more information, visit www.MyBod4God.org"
+)
+#
+# competition2 = Competition.create(
+#   start_date: "1/12/2016",
+#   end_date: "3/30/2016",
+#   location: "Woodstream",
+#   team_names: "Team 1, Team 2, Team 3",
+#   about: "For more information, visit www.MyBod4God.org"
+# )
+
+user = User.create(
+  email: "mail@MichaelLParks.com",
+  password: "Password1234"
+)
+
+# user2 = User.create(
+#   email: "mail2@MichaelLParks.com",
+#   password: "Password1234"
+# )
+#
+# user3 = User.create(
+#   email: "mail3@MichaelLParks.com",
+#   password: "Password1234"
+# )
+
+12.times do |i|
+  user.weigh_ins.create(competition_id: competition.id)
+end
